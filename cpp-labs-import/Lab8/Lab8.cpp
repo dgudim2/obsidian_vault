d9b1764776901d35d5a2d5e31a2e02cb78841a32
kloud_xps13 <dgudim@gmail.com>
2021-11-25 12:01:45 +0300

    Added print function

--- cpp-labs-import/Lab8/Lab8.cpp
+++ cpp-labs-import/Lab8/Lab8.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 using namespace std;
 
+string file_path = "C:\\Users\\kloud\\Downloads\\entries.bin";
+
 enum lessons {
     PHYSICS = 0,
     MATH = 1,
@@ -47,7 +49,7 @@ void inputEntry(student_entry *entry) {
 }
 
 void write_entries(student_entry* entries , unsigned int size) {
-    ofstream file("C:\\Users\\kloud\\Downloads\\entries.bin", ios::out | ios::binary);
+    ofstream file(file_path, ios::out | ios::binary);
     file.write(reinterpret_cast<char*>(&size), sizeof(unsigned int));
     for (unsigned int i = 0; i < size; i++) {
         // determine the size of the string
@@ -59,7 +61,7 @@ void write_entries(student_entry* entries , unsigned int size) {
 
         file.write(reinterpret_cast<char*>(&entries[i].year_of_birth), sizeof(unsigned int));
         file.write(reinterpret_cast<char*>(&entries[i].group), sizeof(unsigned int));
-
+        //TODO use size_type instead of unsigned int, save lessons_size at the beginning
         file.write(reinterpret_cast<char*>(&lessons_size), sizeof(unsigned int));
         for (unsigned int e = 0; e < lessons_size; e++) {
             unsigned int vector_size = entries[i].grades[(lessons)e].size();
@@ -68,15 +70,66 @@ void write_entries(student_entry* entries , unsigned int size) {
                 file.write(reinterpret_cast<char*>(&entries[i].grades[(lessons)e].at(m)), sizeof(unsigned int));
             }
         }
-        file.write(reinterpret_cast<char*>(&entries[i].grades_average), sizeof(unsigned int));
+        file.write(reinterpret_cast<char*>(&entries[i].grades_average), sizeof(float));
     }
     file.flush();
     file.close();
 }
 
-//student_entry* read_entries() {
-//
-//}
+student_entry* read_entries() {
+    unsigned int size = 0;
+    student_entry* students;
+    ifstream file;
+    file.open(file_path, ios::binary);
+    file.read(reinterpret_cast<char*>(&size), sizeof(unsigned int));
+    students = new student_entry[size];
+
+    for (unsigned int i = 0; i < size; i++) {
+        student_entry student;
+
+        string::size_type sz = 0;
+        file.read(reinterpret_cast<char*>(&sz), sizeof(string::size_type));
+        student.fio.resize(sz);
+        file.read(&student.fio[0], sz);
+        file.read(reinterpret_cast<char*>(&student.year_of_birth), sizeof(unsigned int));
+        file.read(reinterpret_cast<char*>(&student.group), sizeof(unsigned int));
+
+        unsigned int lessons_count = 0;
+        file.read(reinterpret_cast<char*>(&lessons_count), sizeof(unsigned int));
+        for (unsigned int e = 0; e < lessons_count; e++) {
+            unsigned int vector_size = 0;
+            file.read(reinterpret_cast<char*>(&vector_size), sizeof(unsigned int));
+            for (unsigned int v = 0; v < vector_size; v++) {
+                unsigned int grade = 0;
+                file.read(reinterpret_cast<char*>(&grade), sizeof(unsigned int));
+                student.grades[(lessons)e].push_back(grade);
+            }
+        }
+
+        file.read(reinterpret_cast<char*>(&student.grades_average), sizeof(float));
+
+        students[i] = student;
+    }
+    file.close();
+    return students;
+}
+
+void printEntries(student_entry* students, unsigned int size) {
+    coutWithColor(4, "Количество студентов: " + to_string(size) + "\n");
+    for (unsigned int i = 0; i < size; i++) {
+        coutWithColor(7, to_string(i + 1) + "\nФ.И.О: " + students[i].fio);
+        coutWithColor(7, "\nГод рождения: " + to_string(students[i].year_of_birth));
+        coutWithColor(7, "\nНомер группы: " + to_string(students[i].group));
+        
+        for (unsigned int g = 0; g < lessons_size; g++) {
+            unsigned int grades_size = students[i].grades.size();
+            cout << "\nОтметки по " << lessons_map_case[g] << ": " << endl;
+            for (unsigned int v = 0; v < grades_size; v++) {
+                cout << students[i].grades[(lessons)g].at(v);
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -86,6 +139,8 @@ int main()
     
     inputEntry(&entries[0]);
     write_entries(entries, 1);
+
+    printEntries(read_entries(), 1);
 
     while (true) {
         switch (displaySelection(new string[5]{ "1.Создать файл", "2.Просмотреть", "3.Сортировать по среднему баллу", "4.Вывод по фильтру", "5.Выйти" }, 5)) {

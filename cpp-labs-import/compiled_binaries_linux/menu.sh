d2ba81744b0406bcc3ef63e069ad063b4b717a5f
kloud-linux-xps13 <dgudim@gmail.com>
2022-02-28 20:01:11 +0300

    some tweaks

--- cpp-labs-import/compiled_binaries_linux/menu.sh
+++ cpp-labs-import/compiled_binaries_linux/menu.sh
@@ -8,9 +8,12 @@ printf "\033[0m"
 FILES=$(ls -I '*.*')
 CHOICES=""
 
+i=0
+
 for file in $FILES 
 do
-	CHOICES="$CHOICES $file #"
+	i=$((i+1))
+	CHOICES="$CHOICES $file #$i"
 done
 
 result=$(whiptail --title "Menu" --menu "Select a program" 25 78 16 $CHOICES 3>&1 1>&2 2>&3)

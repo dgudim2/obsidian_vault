c3f01a4c782ba88b1bcc1d89ffc1cf69648dd09f
kloud-linux-xps13 <dgudim@gmail.com>
2022-04-08 13:43:15 +0300

    multigraph support

--- cpp-labs-import/sem2_function_roots/sem2_function_roots.cpp
+++ cpp-labs-import/sem2_function_roots/sem2_function_roots.cpp
@@ -3,20 +3,30 @@
 #include "../genericFunctions.h"
 using namespace std;
 
+typedef double (*FunctionPointer)(double);
+
 double findRootOnInterval(double a, double b, double e);
 
 double F(double x) {
     return log(x) - 5 * sin(x) * sin(x);
 }
 
-void printGraph(double from, double to, double step) {
-    unsigned int field_size = 45;
-    unsigned int points = (int)((to - from) / step + 1);
+double F2(double x) {
+    return 4 * x - 7 * sin(x);
+}
 
-    double* x = new double[points];
-    double* y = new double[points];
+double level(double x) {
+    return 0;
+}
 
-    unsigned int point = 0;
+constexpr int field_size = 120;
+
+void printGraph(vector<FunctionPointer> graphs, double from, double to, double step) {
+
+    vector<vector<double>> x_points;
+    vector<vector<double>> y_points;
+
+    unsigned int point;
 
     double minX = from;
     double maxX = to;
@@ -24,56 +34,66 @@ void printGraph(double from, double to, double step) {
     double minY = std::numeric_limits<double>::infinity();
     double maxY = -std::numeric_limits<double>::infinity();
 
-    for (long double i = from; i * (step < 0 ? -1 : 1) <= to * (step < 0 ? -1 : 1); i += step) {
-        x[point] = i;
-        y[point] = F(i);
-        maxY = max(maxY, y[point]);
-        minY = min(minY, y[point]);
-        point++;
+    for (int g = 0; g < graphs.size(); g++) {
+        vector<double> x;
+        vector<double> y;
+        point = 0;
+        for (long double i = from; i * (step < 0 ? -1 : 1) <= to * (step < 0 ? -1 : 1); i += step) {
+            x.push_back(i);
+            y.push_back(graphs[g](i));
+            maxY = max(maxY, y.back());
+            minY = min(minY, y.back());
+            point++;
+        }
+        x_points.push_back(x);
+        y_points.push_back(y);
     }
 
-    for (unsigned int p = 0; p < point; p++) {
-        x[p] -= minX;
-        x[p] /= (maxX - minX);
-        x[p] *= (field_size - 1);
+    for (int g = 0; g < graphs.size(); g++) {
+        for (unsigned int p = 0; p < point; p++) {
+            x_points[g][p] -= minX;
+            x_points[g][p] /= (maxX - minX);
+            x_points[g][p] *= (field_size - 1);
 
-        y[p] -= minY;
-        y[p] /= (maxY - minY);
-        y[p] *= (field_size - 1);
+            y_points[g][p] -= minY;
+            y_points[g][p] /= (maxY - minY);
+            y_points[g][p] *= (field_size - 1);
+        }
     }
 
-    bool** field = new bool* [field_size];
+    int** field = new int* [field_size];
     for (unsigned int i = 0; i < field_size; i++) {
-        field[i] = new bool[field_size];
+        field[i] = new int[field_size];
     }
 
     for (unsigned int x_coord = 0; x_coord < field_size; x_coord++) {
         for (unsigned int y_coord = 0; y_coord < field_size; y_coord++) {
-            field[x_coord][y_coord] = false;
+            field[x_coord][y_coord] = 0;
         }
     }
 
-    for (unsigned int p = 0; p < point - 1; p++) {
-        unsigned int steps = max(abs((int)x[p] - (int)x[p + 1]), abs((int)y[p] - (int)y[p + 1])) + 1;
-        if (steps > 1) {
-            for (unsigned int i = 0; i < steps; i++) {
-                field[(int)lerp(x[p], x[p + 1], i / (double)(steps - 1))][(int)lerp(y[p], y[p + 1], i / (double)(steps - 1))] = true;
+    for (int g = 0; g < graphs.size(); g++) {
+        for (unsigned int p = 0; p < point - 1; p++) {
+            unsigned int steps = max(abs((int)x_points[g][p] - (int)x_points[g][p + 1]), abs((int)y_points[g][p] - (int)y_points[g][p + 1])) + 1;
+            if (steps > 1) {
+                for (unsigned int i = 1; i < steps; i++) {
+                    field[(int)lerp(x_points[g][p], x_points[g][p + 1], i / (double)(steps - 1))][(int)lerp(y_points[g][p], y_points[g][p + 1], i / (double)(steps - 1))] += (g + 1);
+                }
+            } else {
+                field[(int)x_points[g][p]][(int)y_points[g][p]] += (g + 1);
             }
-        } else {
-            field[(int)x[p]][(int)y[p]] = true;
-        }
 
+        }
     }
 
     for (unsigned int y_coord = 0; y_coord < field_size; y_coord++) {
         for (unsigned int x_coord = 0; x_coord < field_size; x_coord++) {
-            cout << (field[x_coord][field_size - y_coord - 1] ? "██" : "░░");
+            coutWithColor(mapToColor(field[x_coord][field_size - y_coord - 1], 0, graphs.size() + 1),
+                field[x_coord][field_size - y_coord - 1] > 0 ? "██" : "░░");
         }
         cout << endl;
     }
 
-    delete[] x;
-    delete[] y;
     for (unsigned int i = 0; i < field_size; ++i) {
         delete[] field[i];
     }
@@ -82,8 +102,14 @@ void printGraph(double from, double to, double step) {
 
 int main() {
     double a = 2, b = 6, e = 0.0001;
-    printGraph(a, b, (b - a) / 45);
-    double res = findRootOnInterval(a, b, e);
+    printGraph({ F2, F, level }, a, b, (b - a) / (double)field_size);
+    double res;
+    for (double i = a; i < b; i += (b - a) / 5) {
+        res = findRootOnInterval(i, i + (b - a) / 5, e);
+        if (abs(F(res)) < abs(F(res) - F(res + e))) {
+            cout << res << endl;
+        }
+    }
     _getch();
 
     return 0;

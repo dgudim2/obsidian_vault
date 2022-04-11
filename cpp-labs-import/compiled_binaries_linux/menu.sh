d1204bda37f741f7cd7f129897fd187de656734e
kloud-linux-xps13 <dgudim@gmail.com>
2022-04-11 09:28:30 +0300

    menu

---
+++ cpp-labs-import/compiled_binaries_linux/menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ..
+./compile_all.sh
+cd compiled_binaries_linux
+
+printf "\033[0m"
+
+FILES=$(ls -I '*.*' -t)
+CHOICES=""
+
+i=0
+
+for file in $FILES 
+do
+	i=$((i+1))
+	CHOICES="$CHOICES $file #$i"
+done
+
+result=$(whiptail --title "Menu" --menu "Select a program" 25 78 16 $CHOICES 3>&1 1>&2 2>&3)
+
+clear
+./$result
+clear

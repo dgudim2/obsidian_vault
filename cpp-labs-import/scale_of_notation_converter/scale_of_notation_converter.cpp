7d006766c56b462eadf04ab5cc97c341d0816ebb
kloud-sauron <dgudim@gmail.com>
2022-10-23 14:16:20 +0300

    improved scale of notation converter

--- cpp-labs-import/scale_of_notation_converter/scale_of_notation_converter.cpp
+++ cpp-labs-import/scale_of_notation_converter/scale_of_notation_converter.cpp
@@ -3,27 +3,67 @@
 #include <string>
 using namespace std;
 
-char conversion_arr[36]{ '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+// 36
+string conversion_arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-string& convert (int num, int scale_of_notation, string& buf) {
+int getNumSignificantDigits(int radix, double eps) {
+    return (int)ceil(log(1 / eps) / log(radix));
+}
+
+string& convertInt(int num, int radix, string& buf) {
     if (num == 0) {
         return buf;
     }
-    int remainder = num % scale_of_notation;
+    int remainder = num % radix;
     buf.insert(0, 1, conversion_arr[remainder]);
-    return convert(num / scale_of_notation, scale_of_notation, buf);
+    return convertInt(num / radix, radix, buf);
 }
 
-int main()
-{
-    int num = (int)inputDouble("Введите число: ");
-    int scale_of_notation = (int)inputDouble("Введите систему систему счисления: ");
-
-    if (scale_of_notation >= 35 || scale_of_notation < 2) {
-        cout << "Прикалываетесь?, перевожу в двоичную\n";
-        scale_of_notation = 2;
+string& convertFract(double num, int radix, int num_digits, string& buf) {
+    if (num_digits == 0) {
+        return buf;
     }
-    
-    string str("");
-    cout << "Ваше число в " << scale_of_notation << "-ичной системе счисления: " << convert(num, scale_of_notation, str);
+    double mul = num * radix;
+    int int_part = floor(mul);
+    double frac_part = mul - int_part;
+
+    buf += conversion_arr[int_part];
+    return convertFract(frac_part, radix, num_digits - 1, buf);
+}
+
+int main() {
+    coutWithColor(colors::LIGHT_GREEN, "Input initial data:\n");
+    double num = inputDouble("    Initial decimal number: ");
+    int radix = (int)inputDouble("    Target radix: ");
+    double eps = inputDouble("    Target precision (epsilon_sup): ");
+
+    cout << "\n";
+
+    int int_part = floor(num);
+    double frac_part = num - int_part;
+
+    if (radix >= 35 || radix < 2) {
+        cout << "Invalid radix, converting to binary\n";
+        radix = 2;
+    }
+
+    string str;
+    string str2;
+
+    int num_digits = getNumSignificantDigits(radix, eps);
+
+    coutWithColor(colors::YELLOW, "Results:\n");
+    cout << "    Number of significant digits: ";
+    coutWithColor(colors::LIGHT_PURPLE, to_string(num_digits));
+    cout << " (corresponds to epsilon_sup: ";
+    coutWithColor(colors::LIGHT_BLUE, to_string(eps));
+    cout << ")\n";
+
+    cout << "    The result of converting: ";
+    coutWithColor(colors::LIGHT_GREEN, to_string(num) + "₁₀");
+    cout << " to radix ";
+    coutWithColor(colors::PURPLE, to_string(radix));
+    cout << ": ";
+    coutWithColor(colors::GREEN, convertInt(int_part, radix, str) + "," + convertFract(frac_part, radix, num_digits, str2));
+    waitForButtonInput("\n\nPress any button to close the window...");
 }

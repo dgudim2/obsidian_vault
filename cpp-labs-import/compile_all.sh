bfbf977f1a606b0447e56fe076fa807140711c4a
Kloud-xps-arch <dgudim@gmail.com>
2022-11-09 17:51:32 +0200

    compile commands generation

--- cpp-labs-import/compile_all.sh
+++ cpp-labs-import/compile_all.sh
@@ -2,8 +2,13 @@
 
 skipped=0
 
+COMPILE_COMMANDS=compile_commands.json
+
 touch compiled_tmp
 touch failed_tmp
+
+# start compile commands file
+echo "[" > $COMPILE_COMMANDS
 
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
@@ -25,19 +30,31 @@ if ! grep -q $hash_generic_functions "header_hash"; then
 
 fi
 
+
 for elem in $(find ./$1 -type f -iregex ".*\.cpp")
 do
     name=$(echo "$elem" | cut -f2 -d'/')
     printf "${YELLOW}compiling: $name\n${NC}"
     hash=$(md5sum $elem | cut -d" " -f1)
     
+    COMMAND=/usr/bin/clang++\ -std=c++17\ $elem\ -o\ ./compiled_binaries_linux/$name\ -I\ $name\ -I\ .\ $(cat $name/params 2> /dev/null | tr -d '\n')
+
+    if [ ! -z ${ADD_COMA+x} ]; then
+        echo , >> $COMPILE_COMMANDS
+    fi
+    ADD_COMA=1
+
+    echo '{ "directory": "./compiled_binaries_linux", ' >> $COMPILE_COMMANDS
+    echo "  \"command\": \"${COMMAND}\"," >> $COMPILE_COMMANDS
+    echo "  \"file\": \"${elem}\" } " >> $COMPILE_COMMANDS
+
     if grep -q $hash "$name/hash"; then
         printf " ${MAGENTA}up-to-date, skipping\n"
         skipped=$((skipped+1))
         continue
     fi
-    
-    ( if clang++ -std=c++17 $elem -o ./compiled_binaries_linux/$name -I $name -I . $(cat $name/params 2> /dev/null | tr -d '\n') ; then
+
+    ( if $COMMAND ; then
             printf "${GREEN}compiled: $name\n${NC}"
             echo -n "c" >> compiled_tmp
             echo "$hash" > $name/hash
@@ -55,6 +72,9 @@ failed=$(wc -m < failed_tmp)
 
 rm compiled_tmp
 rm failed_tmp
+
+# end compile commands file
+echo "]" >> $COMPILE_COMMANDS
 
 printf "\n${NC}$compiled ${GREEN}compiled${NC}, $skipped ${MAGENTA}up to date${NC}, $failed ${RED}failed\n"
 if (($failed > 0)); then

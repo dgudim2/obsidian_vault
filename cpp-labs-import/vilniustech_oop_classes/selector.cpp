7fb56dc0236759f793e42ec47dddc9f03e6258d5
kloud-sauron <dgudim@gmail.com>
2023-03-24 19:34:53 +0200

    more labs

--- cpp-labs-import/vilniustech_oop_classes/selector.cpp
+++ cpp-labs-import/vilniustech_oop_classes/selector.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include <math.h>
 #include <iomanip>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <set>
 
 using namespace std;
 
@@ -23,14 +25,11 @@ class Triangle {
     double a, b, c;
     bool print_detroy;
 
-    public:
-    Triangle(double a, double b, double c, bool print_detroy = false): a(a), b(b), c(c), print_detroy(print_detroy) {
+  public:
+    Triangle(double a, double b, double c, bool print_detroy = false)
+        : a(a), b(b), c(c), print_detroy(print_detroy) {}
 
-    }
-
-    Triangle(bool print_detroy = false) : Triangle(6, 6, 6, print_detroy) {
-        
-    }
+    Triangle(bool print_detroy = false) : Triangle(6, 6, 6, print_detroy) {}
 
     double getArea() {
         double s = (a + b + c) / 2;
@@ -41,6 +40,27 @@ class Triangle {
         if (print_detroy) {
             cout << "destroy\n";
         }
+    }
+};
+
+double distanceBetweenPoints(double x, double y, double x1, double y1) {
+    return sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+}
+
+class Circle {
+  private:
+    double x, y, r;
+
+  public:
+    Circle(double x, double y, double r) : x(x), y(y), r(r) {}
+
+    bool hasPoint(double p_x, double p_y) {
+        return distanceBetweenPoints(x, y, p_x, p_y) <= r;
+    }
+
+    bool intersects(const Circle &c) {
+        double distance = distanceBetweenPoints(x, y, c.x, c.y);
+        return distance <= (r + c.r) && distance >= r - c.r && distance >= c.r - r;
     }
 };
 
@@ -77,7 +97,7 @@ int _3_area_tringles() {
 int _4_perimeter_area_rectangles() {
     int mode, w, h;
     cin >> mode >> w >> h;
-    if(mode == 1) {
+    if (mode == 1) {
         cout << Rect(w, h).getArea() + Rect().getArea();
     } else {
         cout << Rect(w, h).getPerimeter() + Rect().getPerimeter();
@@ -85,9 +105,70 @@ int _4_perimeter_area_rectangles() {
     return 0;
 }
 
-int main() {
+int _5_area_triangles_comparison() {
     int a, b, c;
     cin >> a >> b >> c;
-    cout << ((Triangle(a, b, c, true).getArea() > Triangle(true).getArea()) ? "first" : "second");
+    Triangle tr1(a, b, c, true);
+    Triangle tr2(true);
+
+    int area1 = tr1.getArea();
+    int area2 = tr2.getArea();
+
+    cout << ((area1 > area2) ? "first\n" : (area1 == area2 ? "equal\n" : "second\n"));
+    return 0;
+}
+
+int _6_circle_point_in_area() {
+    int n, p = 0, x, y, d;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y >> d;
+        p += Circle(x, y, d / 2.0).hasPoint(0, 0);
+    }
+    cout << p;
+    return 0;
+}
+
+int _7_cicrle_intersection() {
+    Circle c(6, 6, 6);
+    int n, p = 0, x, y, d;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y >> d;
+        p += Circle(x, y, d / 2.0).intersects(c);
+    }
+    cout << p;
+    return 0;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    map<string, int> mapp;
+    set<int> score_set;
+    set<string> disqual_set;
+    for (int i = 0; i < n; i++) {
+        string name;
+        int m1, m2, m3;
+        cin >> name >> m1 >> m2 >> m3;
+        int score = m1 * 5 + m2 * 3 + m3 * 15;
+        mapp.insert(pair(name, score));
+        if (!score_set.insert(score).second) {
+            disqual_set.insert(name);
+        }
+    }
+    string max_name;
+    int max_score = -1;
+    for (auto &[name, score] : mapp) {
+        if (score > max_score && !disqual_set.contains(name)) {
+            max_name = name;
+            max_score = score;
+        }
+    }
+    if (max_score == -1) {
+        cout << "all disqualified";
+    } else {
+        cout << max_name;
+    }
     return 0;
 }

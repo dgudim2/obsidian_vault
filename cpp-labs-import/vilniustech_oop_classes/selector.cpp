b0442dd4401152e0b3b10b2b630cab4b1888be25
kloud-sauron <dgudim@gmail.com>
2023-03-25 11:36:56 +0200

    more labs

--- cpp-labs-import/vilniustech_oop_classes/selector.cpp
+++ cpp-labs-import/vilniustech_oop_classes/selector.cpp
@@ -1,8 +1,10 @@
 #include <iomanip>
 #include <iostream>
+#include <limits.h>
 #include <map>
 #include <math.h>
 #include <set>
+#include <vector>
 
 using namespace std;
 
@@ -36,6 +38,21 @@ class Triangle {
         return sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
+    bool isValid() { return !(a + b <= c || a + c <= b || b + c <= a); }
+
+    bool isObtuse() {
+        double longestSize = max(max(a, b), c);
+        double shortSidesSquare;
+        if (longestSize == a) {
+            shortSidesSquare = b * b + c * c;
+        } else if (longestSize == b) {
+            shortSidesSquare = a * a + c * c;
+        } else {
+            shortSidesSquare = b * b + a * a;
+        }
+        return shortSidesSquare < longestSize * longestSize;
+    }
+
     ~Triangle() {
         if (print_detroy) {
             cout << "destroy\n";
@@ -45,6 +62,10 @@ class Triangle {
 
 double distanceBetweenPoints(double x, double y, double x1, double y1) {
     return sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+}
+
+bool collinear(int x1, int y1, int x2, int y2, int x3, int y3) {
+    return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2);
 }
 
 class Circle {
@@ -141,7 +162,7 @@ int _7_cicrle_intersection() {
     return 0;
 }
 
-int main() {
+int _19_mushrooms_BAD() {
     int n;
     cin >> n;
     map<string, int> mapp;
@@ -170,5 +191,110 @@ int main() {
     } else {
         cout << max_name;
     }
+    return 0;
+}
+
+int _8_triangles_obtuse() {
+    int n, x1, x2, x3, y1, y2, y3, n_obtuse = 0, n_valid = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
+        if (collinear(x1, y1, x2, y2, x3, y3)) {
+            continue;
+        }
+        double side1 = distanceBetweenPoints(x1, y1, x2, y2);
+        double side2 = distanceBetweenPoints(x2, y2, x3, y3);
+        double side3 = distanceBetweenPoints(x3, y3, x1, y1);
+        Triangle tr(side1, side2, side3);
+        if (tr.isValid()) {
+            n_valid++;
+            n_obtuse += tr.isObtuse();
+        }
+    }
+    if (n_valid == 0) {
+        cout << "NA";
+    } else {
+        cout << n_obtuse;
+    }
+    return 0;
+}
+
+void printTime(long time) {
+    int hours, minutes, seconds;
+    hours = time / 3600;
+    time -= hours * 3600;
+    minutes = time / 60;
+    time -= minutes * 60;
+    seconds = time;
+    cout << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes
+         << ":" << setw(2) << setfill('0') << seconds << "\n";
+}
+
+long HourMinSecToSec(int hours, int minutes, int seconds) {
+    return (hours * 60 + minutes) * 60 + seconds;
+}
+
+int _9_time_division() {
+    int hours, minutes, seconds, n_divisions;
+    long time;
+    cin >> n_divisions >> hours >> minutes >> seconds;
+    time = HourMinSecToSec(hours, minutes, seconds);
+
+    for (int i = 0; i < n_divisions; i++) {
+        time /= 3;
+        printTime(time);
+    }
+
+    return 0;
+}
+
+int _10_max_time_sum() {
+    int hours, minutes, seconds, n;
+    cin >> n;
+    long max_time = 0;
+    vector<long> times(n);
+    for (int i = 0; i < n; i++) {
+        cin >> hours >> minutes >> seconds;
+        long time = HourMinSecToSec(hours, minutes, seconds);
+        times[i] = time;
+    }
+    for (int i = 0; i < times.size() - 1; i++) {
+        for (int i2 = i + 1; i2 < times.size(); i2++) {
+            long time = times[i] + times[i2];
+            time -= (time / (24L * 3600)) * 24L * 3600;
+            if (time > max_time) {
+                max_time = time;
+            }
+        }
+    }
+    printTime(max_time);
+    return 0;
+}
+
+int _11_time_delta() {
+    int hours, minutes, seconds, n;
+    cin >> n;
+    long min_delta = INT_MAX;
+    vector<long> times(n);
+    for (int i = 0; i < n; i++) {
+        cin >> hours >> minutes >> seconds;
+        long time = HourMinSecToSec(hours, minutes, seconds);
+        times[i] = time;
+    }
+    const long _24H = 24 * 3600;
+    for (int i = 0; i < times.size() - 1; i++) {
+        for (int i2 = i + 1; i2 < times.size(); i2++) {
+            long delta = abs(times[i] - times[i2]);
+
+            // handle time overflows
+            delta = min(delta, times[i2] - times[i] + _24H);
+            delta = min(delta, times[i] - times[i2] + _24H);
+
+            if (delta < min_delta) {
+                min_delta = delta;
+            }
+        }
+    }
+    printTime(min_delta);
     return 0;
 }

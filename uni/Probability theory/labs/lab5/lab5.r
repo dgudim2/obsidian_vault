2fd81b30d6a72655792a8c8dacecde0da2752008
dgudim2 <dgudim200@gmail.com>
2023-11-27 13:40:24 +0200

    Added task 2, 3 and 4

--- uni/Probability theory/labs/lab5/lab5.r
+++ uni/Probability theory/labs/lab5/lab5.r
@@ -3,6 +3,24 @@ print(getwd())
 data <- read.table("mydata.txt")
 print(data)
 
+# Task 2: 500 binomial values
+values <- rbinom(500, 10, 0.75)
+print(table(values))
+barplot(table(values))
+
+# Task 3: 500 normal values
+n_values <- rnorm(500, 1, 5)
+hist(n_values)
+print(cut(n_values, 10))
+
+# Task 4: load orange dataset
+data("Orange")
+
+print(str(Orange))
+print(quantile(Orange$age, 0.5))
+pie(table(Orange$Tree), col = rainbow(5, alpha = 0.3))
+hist(Orange$circumference)
+
 # Task 5: read real dataset
 dataset <- read.table("data_LW2.txt")
 random_sel <- dataset[sample(nrow(dataset), 250, 0), ]
@@ -13,10 +31,5 @@ print(var(random_sel$year))
 print(var(random_sel$area))
 print(var(random_sel$price))
 
-# Task 4: load orange dataset
-data("Orange")
 
-print(str(Orange))
-print(quantile(Orange$age, 0.5))
-pie(table(Orange$Tree), col = rainbow(5, alpha = 0.3))
-hist(Orange$circumference)
+

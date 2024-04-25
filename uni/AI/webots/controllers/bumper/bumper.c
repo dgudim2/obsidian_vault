ebe55ac55ec91bf766c512b6532dede92a086dd0
dgudim2 <dgudim200@gmail.com>
2024-04-25 23:06:37 +0300

    Improve AI webots robot

--- uni/AI/webots/controllers/bumper/bumper.c
+++ uni/AI/webots/controllers/bumper/bumper.c
@@ -20,30 +20,35 @@
 
 #include <webots/motor.h>
 #include <webots/robot.h>
+#include <webots/speaker.h>
 #include <webots/touch_sensor.h>
 
 #define SPEED 4
 #define TIME_STEP 64
 
 int main() {
-  WbDeviceTag bumper;
-  WbDeviceTag left_motor, right_motor;
+  WbDeviceTag bumper, speaker;
+  WbDeviceTag left_motor, right_motor, prop_motor;
   int movement_counter = 0;
-  int left_speed = 0, right_speed = 0;
+  int left_speed = 0, right_speed = 0, prop_speed = 0, prop_time = 0;
 
   wb_robot_init();
 
   /* get a handle the the bumper and activate it. */
   bumper = wb_robot_get_device("bumper");
+  speaker = wb_robot_get_device("speaker");
   wb_touch_sensor_enable(bumper, TIME_STEP);
 
   /* get a handler to the motors and set target position to infinity (speed control) */
   left_motor = wb_robot_get_device("left wheel motor");
   right_motor = wb_robot_get_device("right wheel motor");
+  prop_motor = wb_robot_get_device("prop motor");
   wb_motor_set_position(left_motor, INFINITY);
   wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_position(prop_motor, INFINITY);
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
+  wb_motor_set_velocity(prop_motor, 0.0);
 
   /* control loop */
   while (wb_robot_step(TIME_STEP) != -1) {
@@ -63,11 +68,12 @@ int main() {
     if (movement_counter <= 0) {
       left_speed = SPEED - movement_counter / 3;
       right_speed = SPEED - movement_counter / 3;
-      movement_counter--;
 
+      movement_counter--;
     } else if (movement_counter >= 15) {
       left_speed = -movement_counter / 3;
       right_speed = -movement_counter / 3;
+
       movement_counter--;
     } else {
       left_speed = -SPEED - movement_counter / 5;
@@ -76,9 +82,18 @@ int main() {
       movement_counter--;
     }
 
+    prop_time ++;
+    if(prop_time > 250) {
+      prop_speed = 300;
+      left_speed = 0;
+      right_speed = 0;
+      wb_speaker_play_sound(speaker, speaker, "./sound.wav", 1.0, 1.0, 0, false);
+    }
+
     /* Set the motor speeds. */
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
+    wb_motor_set_velocity(prop_motor, prop_speed);
   }
 
   wb_robot_cleanup();

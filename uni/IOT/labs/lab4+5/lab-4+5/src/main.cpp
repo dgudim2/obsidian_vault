b4195f22df33a7b0bd963cf8677d137ce1a1cc9d
dgudim2 <dgudim200@gmail.com>
2025-03-08 19:42:54 +0200

    Sync

--- uni/IOT/labs/lab4+5/lab-4+5/src/main.cpp
+++ uni/IOT/labs/lab4+5/lab-4+5/src/main.cpp
@@ -19,15 +19,12 @@
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xFF};
 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-// IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "localhost:8080"; // name address for Google (using DNS)
+IPAddress server(10, 21, 46, 14); // IP of the pc with thingsboard running
 
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
+IPAddress ip(10, 21, 46, 68);
 IPAddress myDns(192, 168, 0, 1);
 
 // Initialize the Ethernet client library
@@ -40,22 +37,25 @@ unsigned long beginMicros, endMicros;
 unsigned long byteCount = 0;
 bool printWebData = true; // set to false for better speed measurement
 
-void setup() {
-    // You can use Ethernet.init(pin) to configure the CS pin
-    // Ethernet.init(10);  // Most Arduino shields
-    // Ethernet.init(5);   // MKR ETH shield
-    // Ethernet.init(0);   // Teensy 2.0
-    // Ethernet.init(20);  // Teensy++ 2.0
-    // Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-    // Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+const int trigPin = A3;
+const int echoPin = A4;
 
-    // Open serial communications and wait for port to open:
+void setup() {
+
+    // Ultrasonic sensor setup
+    pinMode(A2, OUTPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(A5, OUTPUT);
+
+    digitalWrite(A5, LOW);
+    digitalWrite(A2, HIGH);
+
     Serial.begin(505000);
     while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB port only
+        ;
     }
 
-    // start the Ethernet connection:
     Serial.println("Initialize Ethernet with DHCP:");
     if (Ethernet.begin(mac) == 0) {
         Serial.println("Failed to configure Ethernet using DHCP");
@@ -77,61 +77,90 @@ void setup() {
     }
     // give the Ethernet shield a second to initialize:
     delay(1000);
+}
+
+long microsecondsToCentimeters(long microseconds) { return microseconds / 29 / 2; }
+
+void loop() {
+    Serial.println("measuring distance...");
+
+    long duration, cm;
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    
+    cm = microsecondsToCentimeters(duration);
+
+    Serial.print("measured distance is ");
+    Serial.print(cm);
+    Serial.println(" cm");
+
     Serial.print("connecting to ");
     Serial.print(server);
     Serial.println("...");
 
+    String json = String("{\"distance\": ") + String(cm) + String(", \"raw\": ") + String(duration) + String("}");
+    Serial.print("Sending json: ");
+    Serial.println(json);
+
     // if you get a connection, report back via serial:
-    if (client.connect(server, 80)) {
+    if (client.connect(server, 8080)) {
         Serial.print("connected to ");
         Serial.println(client.remoteIP());
-        // Make a HTTP request:
+        // Make an HTTP request:
         client.println("POST /api/v1/uwo688l5ki16ipnjvtjd/telemetry HTTP/1.1");
         client.println("Host: localhost");
+        client.println("User-Agent: Arduino/1.0");
+        client.println("Content-Type: application/json;charset=UTF-8");
         client.println("Connection: close");
+        client.print("Content-Length: ");
+        client.println(json.length());
         client.println();
+        client.println(json);
     } else {
         // if you didn't get a connection to the server:
         Serial.println("connection failed");
     }
+
     beginMicros = micros();
-}
+    byteCount = 0;
 
-void loop() {
-    // if there are incoming bytes available
-    // from the server, read them and print them:
-    int len = client.available();
-    if (len > 0) {
-        byte buffer[80];
-        if (len > 80)
-            len = 80;
-        client.read(buffer, len);
-        if (printWebData) {
-            Serial.write(buffer, len); // show in the serial monitor (slows some boards)
-        }
-        byteCount = byteCount + len;
-    }
-
-    // if the server's disconnected, stop the client:
-    if (!client.connected()) {
-        endMicros = micros();
-        Serial.println();
-        Serial.println("disconnecting.");
-        client.stop();
-        Serial.print("Received ");
-        Serial.print(byteCount);
-        Serial.print(" bytes in ");
-        float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-        Serial.print(seconds, 4);
-        float rate = (float)byteCount / seconds / 1000.0;
-        Serial.print(", rate = ");
-        Serial.print(rate);
-        Serial.print(" kbytes/second");
-        Serial.println();
-
-        // do nothing forevermore:
-        while (true) {
-            delay(1);
+    while (client.connected()) {
+        // if there are incoming bytes available
+        // from the server, read them and print them:
+        int len = client.available();
+        if (len > 0) {
+            byte buffer[80];
+            if (len > 80)
+                len = 80;
+            client.read(buffer, len);
+            if (printWebData) {
+                Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+            }
+            byteCount = byteCount + len;
         }
     }
+
+    endMicros = micros();
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    Serial.print("Received ");
+    Serial.print(byteCount);
+    Serial.print(" bytes in ");
+    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
+    Serial.print(seconds, 4);
+    float rate = (float)byteCount / seconds / 1000.0;
+    Serial.print(", rate = ");
+    Serial.print(rate);
+    Serial.print(" kbytes/second");
+    Serial.println();
+    Serial.println();
+    Serial.println();
+    Serial.println();
+
+    delay(1000);
 }
